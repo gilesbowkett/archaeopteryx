ffe84eaf074e84fcc9b2dfe7f7a7ac4854a0bb9a
Giles Bowkett <gilesb@gmail.com>
2008-10-17 18:13:44 -0700

    This is some code which generates melodies from text files.

--- text2tone.rb
+++ text2tone.rb
@@ -1,3 +1,28 @@
+# usage: ruby text2tone.rb muppets
+
 require 'lib/archaeopteryx'
 
-LiveMIDI.new(:clock => Clock.new(101))
+clock = Clock.new(101)
+midi = LiveMIDI.new(:clock => clock, :logging => false)
+
+def note(midi_note_number)
+  Note.create(:channel => 0,
+              :number => midi_note_number,
+              :duration => 0.25,
+              :velocity => 100 + rand(27))
+end
+
+
+file = ARGV[0]
+base_note = 60 # middle C
+
+File.new(file).each_line do |line|
+  line.split.each do |word|
+    midi.play(note(base_note + word.length))
+    clock.tick
+  end
+end
+
+while true
+  gets
+end
