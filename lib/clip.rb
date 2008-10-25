5ca8f8f54fbbb25e189a3c5756656802e52d19dd
Giles Bowkett <gilesb@gmail.com>
2008-10-24 23:19:41 -0700

    comments sketching plan

--- lib/clip.rb
+++ lib/clip.rb
@@ -11,14 +11,25 @@ module Archaeopteryx
     end
     def messages(measure)
       0 == measure % @measures ? [@message] : []
+      # if 0 == @relative_measure % @measures
+      #   [@message]
+      # else
+      #   []
+      #   @relative_measure += 1
+      # end
     end
     def mutate(measure)
     end
+    # def choose
+    #   relative_measure = 1
+    #   etc.
+    # end
     def complete?
       [true, false][rand(2)]
       # current_measure == final_measure
       # BUT! these need to be relative measures in this Clip, not absolute measures in the overall Loop
       # possible that Loop should be called Engine or some crazy shit
+      # I guess relative_measure is really @current_measure
     end
   end
 end
