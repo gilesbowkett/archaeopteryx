7f697d4cf12f8cfcf71e06d10d4b10e92ef01706
Giles Bowkett <gilesb@gmail.com>
2009-05-05 01:26:15 -0700

    moved

---
+++ drums_live.rb
@@ -0,0 +1,15 @@
+require 'lib/archaeopteryx'
+
+@clock = Clock.new(170)
+
+@loop = Loop.new(:clock => @clock,
+                 :measures => 4,
+                 :logging => false,
+                 :evil_timer_offset_wtf => 0.2,
+                 :midi => LiveMIDI.new(:clock => @clock, # meh
+                                       :logging => false,
+                                       :midi_destination => 0),
+                 :generator => Rhythm.new(:drumfile => "db_drum_definition.rb",
+                                          :mutation => L{|measure| 0 == (measure - 1) % 2}))
+@loop.go
+gets
