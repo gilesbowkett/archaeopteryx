d5c19c49db20f18d144501a9b2b7ed2ef8a12a4e
Giles Bowkett <gilesb@gmail.com>
2009-05-05 13:59:31 -0700

    well that was pretty fucking weird

--- spec/midi_files_spec.rb
+++ spec/midi_files_spec.rb
@@ -22,6 +22,8 @@ describe FileMIDI do
     it "adds notes to itself" do
       2.times {@midi.play(@create_note[])}
       @midi.instance_variable_get("@track").events.size.should == 2
+      @midi.instance_variable_get("@track").events.collect(&:class).should == [Note, Note]
+      # note: I don't understand how this spec passes - I don't think I'm requiring ActiveSupport anywhere
     end
     it "has a MIDI sequence and track" do
       @midi.instance_variable_get("@sequence").should be_an_instance_of MIDI::Sequence
