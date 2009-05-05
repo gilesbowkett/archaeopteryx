d3f7b555d3e6d4e74646be537b55ba24cb35b0f1
Giles Bowkett <gilesb@gmail.com>
2009-05-05 13:11:59 -0700

    got the ability to retain events, also, I threw out the clock. midilib may not work in such a way that the clock is necessary. it makes sense since the clock exists for real-time shit and this isn't real time anyway.

--- lib/midi/file_output/file_midi.rb
+++ lib/midi/file_output/file_midi.rb
@@ -3,12 +3,17 @@
 # for args and has a method called play() which takes a note argument.
 
 class FileMIDI
-  attr_accessor :clock, :filename
-  def initialize(options)
-    raise :hell unless options.is_a? Hash
-    @clock = options[:clock]
-    @filename = options[:filename]
+  attr_accessor :filename,
+                :events
+  def initialize(filename)
+    @filename = filename
+    @events = []
   end
   def play(note)
+    @events << note
   end
 end
+
+# I think what this has to do is have a ticker. because it expects to tick, and you're using predefined
+# intervals. so if you've got a ticker, you just have to check if it's ticked, or something. actually I
+# have no idea - I don't know how midilib tracks time.
--- spec/midi_files_spec.rb
+++ spec/midi_files_spec.rb
@@ -1,10 +1,26 @@
 require 'lib/archaeopteryx'
 
 describe FileMIDI do
-  it "requires options: a clock and a filename" do
-    FileMIDI.new(:clock => Clock.new(170),
-                 :filename => "foo.mid").should be_an_instance_of FileMIDI
-    L{FileMIDI.new}.should raise_error
-    L{FileMIDI.new("asdf")}.should raise_error
+  describe "things that may really belong to a nonexistent abstract superclass and not this class at all" do
+    it "requires a filename" do
+      FileMIDI.new("foo.mid").should be_an_instance_of FileMIDI
+      L{FileMIDI.new}.should raise_error
+    end
+    it "has a method called play" do
+      L{FileMIDI.new("foo.mid").play(Note.new)}.should_not raise_error
+    end
+  end
+  describe "creating MIDI files" do
+    before(:each) do
+      @midi = FileMIDI.new("foo.mid")
+      @create_note = L{Note.create(:channel => 2,
+                                   :number => 64,
+                                   :duration => 0.25, # notes need better ways to express duration
+                                   :velocity => 127)}
+    end
+    it "adds notes to itself" do
+      2.times {@midi.play(@create_note[])}
+      @midi.events.size.should == 2
+    end
   end
 end
