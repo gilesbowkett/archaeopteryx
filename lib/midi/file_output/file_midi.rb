54dc6e80906fca703a53f1999e62fc82d10383b5
Giles Bowkett <gilesb@gmail.com>
2009-05-05 10:55:48 -0700

    comments and empty method

--- lib/midi/file_output/file_midi.rb
+++ lib/midi/file_output/file_midi.rb
@@ -1,8 +1,14 @@
+# a quick note about FAIL. FileMIDI is actually of the same unspecified superclass as LiveMIDI. both
+# need new, clearer names, but the nonexistent abstract superclass is just a class which takes options
+# for args and has a method called play() which takes a note argument.
+
 class FileMIDI
   attr_accessor :clock, :filename
   def initialize(options)
     raise :hell unless options.is_a? Hash
     @clock = options[:clock]
     @filename = options[:filename]
+  end
+  def play(note)
   end
 end
