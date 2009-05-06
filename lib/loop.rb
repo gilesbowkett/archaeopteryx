b407bd161a792e9d03943a6c4fb0439ff65b03b0
Giles Bowkett <gilesb@gmail.com>
2009-05-06 15:34:15 -0700

    refactor

--- lib/loop.rb
+++ lib/loop.rb
@@ -2,25 +2,29 @@ module Archaeopteryx
   class Loop
     attr_reader :midi
     def initialize(attributes)
-      %w{generator measures beats clock midi evil_timer_offset_wtf}.each do |option|
+      %w{generator
+         measures
+         beats
+         clock
+         midi
+         evil_timer_offset_wtf}.each do |option|
         eval("@#{option} = attributes[:#{option}]")
       end
-      @generate_beats = L do
-        (1..@measures).each do |measure|
-          @generator.mutate(measure)
-          (0..(@beats - 1)).each do |beat|
-            play @generator.notes(beat)
-            @clock.tick
-          end
-        end
-        @midi.timer.at((@clock.start + @clock.time) - @evil_timer_offset_wtf, &@generate_beats)
-      end
     end
+    def generate_beats
+      (1..@measures).each do |measure|
+        @generator.mutate(measure)
+        (0..(@beats - 1)).each do |beat|
+          play @generator.notes(beat)
+          @clock.tick
+        end
+      end
+      @midi.timer.at((@clock.start + @clock.time) - @evil_timer_offset_wtf,
+                     &L{generate_beats})
+    end
+    alias :go :generate_beats
     def play(music)
       music.each {|note| @midi.play(note)}
-    end
-    def go
-      @generate_beats[]
     end
   end
 end
