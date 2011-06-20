3ec7266f618f7d40438f95bf1ec400fba3c5e64b
Giles Bowkett <gilesb@gmail.com>
2011-06-19 18:31:41 -0700

    took out earlier hack bullshit

--- drums_live.rb
+++ drums_live.rb
@@ -1,22 +1,18 @@
 require 'lib/archaeopteryx'
 require 'rbcoremidi/apc20'
 
-$wtf.capture do
+@clock = Clock.new(170)
 
-  @clock = Clock.new(170)
-
-  @loop = Loop.new(:clock => @clock,
-                   :measures => 4,
-                   :beats => 16,
-                   :logging => false,
-                   :evil_timer_offset_wtf => 0.2,
-                   :midi => LiveMIDI.new(:clock => @clock, # meh
-                                         :logging => true,
-                                         :midi_destination => 0),
-                   :generator => Rhythm.new(:drumfile => "db_drum_definition.rb",
-                                            :mutation => L{|measure| 0 == (measure - 1) % 2}))
-  @loop.generate_beats
-  gets
-
-end
+@loop = Loop.new(:clock => @clock,
+                 :measures => 4,
+                 :beats => 16,
+                 :logging => false,
+                 :evil_timer_offset_wtf => 0.2,
+                 :midi => LiveMIDI.new(:clock => @clock, # meh
+                                       :logging => true,
+                                       :midi_destination => 0),
+                 :generator => Rhythm.new(:drumfile => "db_drum_definition.rb",
+                                          :mutation => L{|measure| 0 == (measure - 1) % 2}))
+@loop.generate_beats
+gets
 
