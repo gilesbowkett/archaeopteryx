d26b21a40812e6a39bad57ec2c44220109ab4061
Giles Bowkett <gilesb@gmail.com>
2011-06-19 18:27:10 -0700

    failed attempt at capturing input
    this will almost certainly require a different strategy (which
    fortunately I have already figured out)

--- drums_live.rb
+++ drums_live.rb
@@ -1,16 +1,22 @@
 require 'lib/archaeopteryx'
+require 'rbcoremidi/apc20'
 
-@clock = Clock.new(170)
+$wtf.capture do
 
-@loop = Loop.new(:clock => @clock,
-                 :measures => 4,
-                 :beats => 16,
-                 :logging => false,
-                 :evil_timer_offset_wtf => 0.2,
-                 :midi => LiveMIDI.new(:clock => @clock, # meh
-                                       :logging => true,
-                                       :midi_destination => 0),
-                 :generator => Rhythm.new(:drumfile => "db_drum_definition.rb",
-                                          :mutation => L{|measure| 0 == (measure - 1) % 2}))
-@loop.generate_beats
-gets
+  @clock = Clock.new(170)
+
+  @loop = Loop.new(:clock => @clock,
+                   :measures => 4,
+                   :beats => 16,
+                   :logging => false,
+                   :evil_timer_offset_wtf => 0.2,
+                   :midi => LiveMIDI.new(:clock => @clock, # meh
+                                         :logging => true,
+                                         :midi_destination => 0),
+                   :generator => Rhythm.new(:drumfile => "db_drum_definition.rb",
+                                            :mutation => L{|measure| 0 == (measure - 1) % 2}))
+  @loop.generate_beats
+  gets
+
+end
+
